3df485235bff69fbff8454f9bcd70753ebfe3cea
NamHyoJeong <121777511+hyojeong-nam@users.noreply.github.com>
2023-07-28 14:08:31 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.33 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,14 +1,18 @@
 class Solution {
     public int solution(int n, String control) {
-
+        int answer = 0;
+        
         //string의 length에는 괄호 필수
         for(int i = 0 ; i<control.length() ; i++){
-            switch(control.charAt(i)){
+            char ch = control.charAt(i);
+            
+            switch(ch){
                 case 'w' : n += 1; break;
                 case 's' : n += -1; break;
                 case 'd' : n += 10; break;
                 case 'a' : n += -10; break;
             }
+            
         }
         return n;
     }
